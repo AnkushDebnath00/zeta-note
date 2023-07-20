59b33ce496ab5946fbc0cfb3b457a653c9adc4a2
Ankush Debnath <ankushdebnath680@gmail.com>
2023-07-21 01:09:46 +0530

    modifications

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import "./LandingStyles.css";
+import "./LandingPage.css";
 
 function LandingPage({ history }) {
   const userLogin = useSelector((state) => state.userLogin);
